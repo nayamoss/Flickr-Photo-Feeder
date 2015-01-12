f4d251b80136251b371ccce22bd148551220212f
Naya Moss <naya@lilymint.co>
2015-01-12 01:52:57 -0500

    Flickr Photo Feeder
    Flickr Photo Feeder uses AJAX, JSON, HTML5 and CSS3. This is a project
    done during the course AJAX: AJAX and API’s on teamtreehouse.com.

---
+++ js/flickr.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $('button').click(function () {
+    // highlight the button
+    // not AJAX, just cool looking
+    $("button").removeClass("selected");
+  $(this).addClass("selected");
+    
+    // AJAX 
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    
+    var animal = $(this).text();
+    var flickrOptions = {
+    tags: animal,
+    format: "json"
+    };
+    function displayPhotos(data) {
+      var photoHTML = '<ul>';
+      $.each( data.items, function (i, photo) {
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href=" ' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+  }); //end click
+});//and ready 
